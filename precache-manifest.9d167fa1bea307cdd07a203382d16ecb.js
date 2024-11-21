f977e58eae747cc93da5807c3ef7a5f65edd3d60
Marcelo Bravo <mabc@live.cl>
2024-11-20 22:11:40 -0300

    Updates

---
+++ precache-manifest.9d167fa1bea307cdd07a203382d16ecb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d0a63390759ceea31088f9a18a2daf",
+    "url": "/unsplashcol/index.html"
+  },
+  {
+    "revision": "9bae6544459c0c2a3f90",
+    "url": "/unsplashcol/static/css/main.a11c7b16.chunk.css"
+  },
+  {
+    "revision": "5f65254efbfee52a6e7c",
+    "url": "/unsplashcol/static/js/2.d62bb95b.chunk.js"
+  },
+  {
+    "revision": "8aca329d31ca141c0240",
+    "url": "/unsplashcol/static/js/3.44ff5753.chunk.js"
+  },
+  {
+    "revision": "9bae6544459c0c2a3f90",
+    "url": "/unsplashcol/static/js/main.d4a26d5e.chunk.js"
+  },
+  {
+    "revision": "3de283b027267bde4204",
+    "url": "/unsplashcol/static/js/runtime~main.a376d590.js"
+  },
+  {
+    "revision": "035628bf594e42db98999e874982ceab",
+    "url": "/unsplashcol/static/media/Logo.035628bf.svg"
+  },
+  {
+    "revision": "8fa267ad57e83175ff33d12f4dd82462",
+    "url": "/unsplashcol/static/media/Plus.8fa267ad.svg"
+  },
+  {
+    "revision": "4900ed0172cdabc92ec3418cb89292c4",
+    "url": "/unsplashcol/static/media/Remove.4900ed01.svg"
+  },
+  {
+    "revision": "daebfb4c926dc341e0ec9af102124801",
+    "url": "/unsplashcol/static/media/Search.daebfb4c.svg"
+  },
+  {
+    "revision": "510a1d5e138d6afcd0b9ebeb5aa76dd7",
+    "url": "/unsplashcol/static/media/down arrow.510a1d5e.svg"
+  },
+  {
+    "revision": "a66a36f140c7b76778548f6b3f6f5493",
+    "url": "/unsplashcol/static/media/gradiend-bg.a66a36f1.svg"
+  },
+  {
+    "revision": "762578dec6fc71598a10f4a81d612563",
+    "url": "/unsplashcol/static/media/hero-image.762578de.png"
+  },
+  {
+    "revision": "5d8165d6f873dc65a15c5d2237a8bfcb",
+    "url": "/unsplashcol/static/media/image_not_found.5d8165d6.png"
+  }
+]);
